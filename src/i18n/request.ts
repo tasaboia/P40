aacda21a7056837ae0f06948de094a4e2f7b1cef
dnms.tech <tech.dnms@gmail.com>
2025-02-15 18:01:58 +0000

    removendo codigo desnecessario

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,12 +1,10 @@
 import { getRequestConfig } from "next-intl/server";
-import { routing } from "./routing";
+import { LocaleType, routing } from "./routing";
 
 export default getRequestConfig(async ({ requestLocale }) => {
-  // This typically corresponds to the `[locale]` segment
   let locale = await requestLocale;
 
-  // Ensure that a valid locale is used
-  if (!locale || !routing.locales.includes(locale as any)) {
+  if (!locale || !routing.locales.includes(locale as LocaleType)) {
     locale = routing.defaultLocale;
   }
 
