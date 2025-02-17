5da8cbf94aee17b7cdbed0501baa67a006d1d09d
dnms.tech <tech.dnms@gmail.com>
2025-02-17 23:06:19 +0000

    layout de horarios do turno

--- src/components/custom/turn/turn-item.tsx
+++ src/components/custom/turn/turn-item.tsx
@@ -1,120 +1,11 @@
-// "use client";
-
-// import { useState } from "react";
-
-// import { Helpers } from "@p40/common/utils/helpers";
-// import {
-//   Accordion,
-//   AccordionContent,
-//   AccordionItem,
-//   AccordionTrigger,
-// } from "@p40/components/ui/accordion";
-// import { Avatar, AvatarFallback, AvatarImage } from "@p40/components/ui/avatar";
-// import { PrayerTurnResponse } from "@p40/common/contracts/user/user";
-// import { UserPlus } from "lucide-react";
-
-// export function TurnItem({
-//   weekday,
-//   shift,
-//   turnItens,
-// }: {
-//   turnItens: PrayerTurnResponse[];
-//   weekday: string;
-//   shift: {
-//     startTime: string;
-//     endTime: string;
-//   }[];
-// }) {
-//   const [selectedTurn, setSelectedTurn] = useState<string | null>(null);
-
-//   //se o horario estiver vazio nao espande e seja um botao para adiciona a si mesmo
-//   //duvida precios mudar de componnete?
-//   //nao ele eh um trigger ja ehu m botao.
-//   //so que repciso confirmar meu whatapp pra isso ne (depois)
-
-//   return (
-//     <Accordion
-//       type="single"
-//       collapsible
-//       className="w-full"
-//       onValueChange={(value) => setSelectedTurn(value)}
-//     >
-//       {shift.map((turn) => {
-//         const leaders = turnItens?.find(
-//           (item) => item.startTime == turn.startTime
-//         )?.leaders;
-
-//         return (
-//           <AccordionItem key={turn.startTime} value={turn.startTime}>
-//             <AccordionTrigger>
-//               <div className="flex w-full ">
-//                 {selectedTurn !== turn.startTime ? (
-//                   leaders?.length > 0 ? (
-//                     leaders
-//                       .map((item) => Helpers.getFirstAndLastName(item.name))
-//                       .join(" ")
-//                   ) : (
-//                     <div className="flex gap-2">
-//                       <UserPlus />
-//                       Me inscrever no horário
-//                     </div>
-//                   )
-//                 ) : (
-//                   "ta selecionado"
-//                 )}
-//               </div>
-//             </AccordionTrigger>
-//             <AccordionContent>
-//               {leaders ? (
-//                 leaders.map((leader) => (
-//                   <div
-//                     key={leader.id}
-//                     className="flex max-w-60 gap-2 px-4 pt-4 text-sm"
-//                   >
-//                     <Avatar className="h-12 w-12 rounded-full">
-//                       <AvatarImage
-//                         src={leader.imageUrl}
-//                         alt="Imagem de perfil"
-//                       />
-//                       <AvatarFallback className="rounded-full">
-//                         {Helpers.getInitials(leader.name)}
-//                       </AvatarFallback>
-//                     </Avatar>
-//                     <div className="grid flex-1 text-left text-sm leading-tight">
-//                       <span className="truncate font-semibold">
-//                         {Helpers.getFirstAndLastName(leader.name)}
-//                       </span>
-//                       <span className="truncate text-xs">
-//                         {leader.whatsapp}
-//                       </span>
-//                     </div>
-//                   </div>
-//                 ))
-//               ) : (
-//                 <p>Esta vazio</p>
-//               )}
-//             </AccordionContent>
-//           </AccordionItem>
-//         );
-//       })}
-//     </Accordion>
-//   );
-// }
-
 "use client";
 
 import { useState } from "react";
 
 import { Helpers } from "@p40/common/utils/helpers";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@p40/components/ui/accordion";
-import { Avatar, AvatarFallback, AvatarImage } from "@p40/components/ui/avatar";
+import { Avatar, AvatarImage } from "@p40/components/ui/avatar";
 import { PrayerTurnResponse } from "@p40/common/contracts/user/user";
-import { BellRing, Check, Clock, Plus, UserPlus } from "lucide-react";
+import { MessageCircle, Plus } from "lucide-react";
 import {
   Card,
   CardContent,
@@ -124,14 +15,20 @@ import {
   CardTitle,
 } from "@p40/components/ui/card";
 import { Button } from "@p40/components/ui/button";
-type CardProps = React.ComponentProps<typeof Card>;
+import Link from "next/link";
+import { EventResponse } from "@p40/common/contracts/event/event";
+
 export function TurnItem({
   weekday,
   shift,
   turnItens,
+  userId,
+  event,
 }: {
   turnItens: PrayerTurnResponse[];
   weekday: string;
+  userId: string;
+  event: EventResponse | null;
   shift: {
     startTime: string;
     endTime: string;
@@ -140,12 +37,7 @@ export function TurnItem({
   const [selectedTurn, setSelectedTurn] = useState<string | null>(null);
 
   return (
-    <Accordion
-      type="single"
-      collapsible
-      className="w-full"
-      onValueChange={(value) => setSelectedTurn(value)}
-    >
+    <div>
       {shift.map((turn) => {
         const leaders = turnItens?.find(
           (item) => item.startTime == turn.startTime
@@ -153,49 +45,62 @@ export function TurnItem({
 
         return (
           <Card key={turn.endTime} className="m-3">
-            {/* <CardHeader>
+            <CardHeader>
               <CardTitle>{turn.startTime}</CardTitle>
               <CardDescription>
-                {leaders ? "Esse horário esta vazio" : "3 Líderes por horário"}
+                {leaders
+                  ? `Máximo ${event.maxParticipantsPerTurn} líderes por turno`
+                  : "Esse horário esta vazio"}
               </CardDescription>
-            </CardHeader> */}
-            <CardContent className="grid gap-4 pt-4 ">
-              <div className=" flex items-center space-x-4 rounded-md border p-4">
-                <Clock />
-                <div className="flex-1 space-y-1">
-                  <p className="text-sm font-medium leading-none">
-                    {turn.startTime}
-                  </p>
-                </div>
-              </div>
-              <div>
-                {leaders?.length > 0 &&
-                  leaders.map((leader) => (
-                    <div
-                      key={leader.id}
-                      className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
-                    >
-                      <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
-                      <div className="space-y-1">
-                        <p className="text-sm font-medium leading-none">
-                          {Helpers.getFirstAndLastName(leader.name)}
-                        </p>
-                        <p className="text-sm text-muted-foreground">
-                          {leader.whatsapp}
-                        </p>
-                      </div>
+            </CardHeader>
+
+            {leaders?.length > 0 &&
+              leaders.map((leader) => (
+                <CardContent key={leader.id} className="grid gap-4 ">
+                  <div key={leader.id} className="flex gap-3 items-center">
+                    <Avatar className="h-10 w-10 rounded-full">
+                      <AvatarImage
+                        src={leader.imageUrl}
+                        alt="Imagem de perfil"
+                      />
+                    </Avatar>
+                    <div className="space-y-1">
+                      <p className="text-sm font-medium leading-none">
+                        {Helpers.getFirstAndLastName(leader.name)}
+                      </p>
+
+                      <Link
+                        href={`https://wa.me/${leader.whatsapp.replace(
+                          /\D/g,
+                          ""
+                        )}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-sm text-muted-foreground flex items-center gap-1 hover:text-green-600 transition"
+                      >
+                        {leader.whatsapp}
+                        <MessageCircle size={16} className="text-green-500" />
+                      </Link>
                     </div>
-                  ))}
-              </div>
-            </CardContent>
+                  </div>
+                </CardContent>
+              ))}
             <CardFooter>
-              <Button className="w-full">
-                <Plus /> Se inscrever no horário
-              </Button>
+              {leaders?.length > 0 &&
+              leaders.find((leader) => leader.id == userId) ? (
+                <Button className="w-full" variant="destructive">
+                  <Plus />
+                  Sair do horário
+                </Button>
+              ) : (
+                <Button className="w-full" variant="secondary">
+                  <Plus /> Se inscrever no horário
+                </Button>
+              )}
             </CardFooter>
           </Card>
         );
       })}
-    </Accordion>
+    </div>
   );
 }
--- src/components/custom/week/day-list.tsx
+++ src/components/custom/week/day-list.tsx
@@ -4,6 +4,7 @@ import { TurnItem } from "../turn/turn-item";
 import { EventResponse } from "@p40/common/contracts/event/event";
 import { turnByWeekday } from "@p40/services/event/turn-weekday";
 import Loading from "@p40/app/[locale]/(auth)/schedule/loading";
+import { auth } from "../../../../auth";
 
 export default async function DayList({
   weekday,
@@ -14,11 +15,18 @@ export default async function DayList({
   weekAbbr: string;
   event: EventResponse | null;
 }) {
+  const session = await auth();
   const shift = createTurns(event?.shiftDuration);
   const turnItens = await turnByWeekday(weekday, event.id);
   return (
     <Suspense fallback={<Loading />}>
-      <TurnItem shift={shift} weekday={weekAbbr} turnItens={turnItens} />
+      <TurnItem
+        userId={session.user.id}
+        event={event}
+        shift={shift}
+        weekday={weekAbbr}
+        turnItens={turnItens}
+      />
     </Suspense>
   );
 }
